4f0473243c3d6a8c93e56e38792dfe250fb602e4
Humberto Ponce Chavez <hbeto.pc@hotmail.com>
2019-05-09 14:24:26 -0500

    Directorios acomodados
    En el archivo conexion.php deben cambiar la variable $bd por el nombre de la base de datos que crearon en local

--- LOGIN CON SESIONES/logica/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
- $host = "localhost";
- $user = "root";
- $clave = "";
- $bd = "jomeni";
- $conectar = mysqli_connect($host,$user,$clave,$bd);
- ?>
---
+++ logica/conexion.php
@@ -0,0 +1,7 @@
+<?php
+ $host = "localhost";
+ $user = "root";
+ $clave = "";
+ $bd = "jomeni";
+ $conectar = mysqli_connect($host,$user,$clave,$bd);
+ ?>
