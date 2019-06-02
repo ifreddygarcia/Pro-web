6934dc461887eb1b4de44591d31c5e1bf7fbd009
Freddy <Freddy19Garcia@gmail.com>
2019-06-02 17:18:15 -0500

    algunos comentarios

--- admin/funciones/deleteUsers.php
+++ admin/funciones/deleteUsers.php
@@ -1,13 +1,20 @@
 <?php
+//Freddy
+//Este archivo es la funcion que elimina un usuario
+	//Referencia al archivo para crear conexión
 require '../../conexion/conexion.php';  
 
+//si se detecta el boton como oprimido se ejecuta
 if (isset($_GET['valor'])) {
+	//guardamos el dato en una variable
     $quien=$_GET['valor'];
     if ($quien > 0) {
+    	//con este ciclo obtenemos el nombre del usurio que se mostrara para saber que eliminaste
         $nombre=mysqli_query($conectar,"SELECT user FROM users where id_user=$quien");
         while ($nom= mysqli_fetch_array($nombre)){
         	echo "El usuario ".$nom['user']." fue eliminado con exito";
         }
+        //la consulta que elimina el usuario
         mysqli_query($conectar,"DELETE FROM users where id_user=$quien");
     }
 }
--- admin/views/tablasUsuarios.php
+++ admin/views/tablasUsuarios.php
@@ -1,20 +1,30 @@
 <?php
-require '../../conexion/conexion.php';  
+//freddy
+//Referencia al archivo para crear conexión
+require '../../conexion/conexion.php'; 
+//consulta para obtener la tabla usuarios 
 $consultar=mysqli_query($conectar,"SELECT *from users");
 
 
 ?>
+<!-- eswte div muestra el resultado de los botones de eliminar y editar-->
 <div id="resultado"></div>
+<!-- Empieza el contenedor donde estar la tabla usuarios-->
 <div class="container-fluid">
+                    <!-- cada div tiene una clase de bootstrap que nos ayuda a darle una forma estetica a la tabla-->
                     <div class="row">
+                        <!--tamaño de columan de la tabla -->
                         <div class="col-md-12">
                             <div class="card strpied-tabled-with-hover">
+                                <!-- cabeza de la tabla-->
                                 <div class="card-header ">
                                     <h4 class="card-title">Estos son todos los usuarios disponibles</h4>
                                     <p class="card-category">Como administrador puede hacer lo que quiera</p>
                                 </div>
+                                <!--encabezados de la tabla estos son estaticos -->
                                 <div class="card-body table-full-width table-responsive">
                                     <table class="table table-hover table-striped">
+                                        <!-- Son los titulos de la tabla-->
                                         <thead>
                                             <th>Id</th>
                                             <th>Usuario</th>
@@ -29,7 +39,11 @@ $consultar=mysqli_query($conectar,"SELECT *from users");
                                             <th>Editar</th>
                                             <th>Eliminar</th>
                                         </thead>
+                                        <!--inicia Body de la tabla -->
                                         <tbody>
+                                            <!--Aqui empieza una mezcla con php y html
+                                            incrustamos codigo php para hacer un ciclo que va a ir leyendo el resultado de la consulta que vimos hasta arriba
+                                            ahora el resultado le agregamos las etiquetas html de la tabla para hacer que el navegador los entienda como tl -->
                                     <?php 
                                         while ($extraido= mysqli_fetch_array($consultar)){
                                                 echo '<tr>';
@@ -44,6 +58,8 @@ $consultar=mysqli_query($conectar,"SELECT *from users");
                                                 echo '<td>'.$extraido['created_at'].'</td>';
                                                 echo '<td>'.$extraido['updated_at'].'</td>';
 
+                                                //aqui va el boton de editar el cual tambien tiene un script que hace que nos llame al documento php que nos mostrara
+                                                // la parte de editar, esto lo mostrara dentro del div de resultado
                                                 echo '<td>
                                                 <button id="11'.$extraido['id_user'].'" type="submit" value="'.$extraido['id_user'].'" class="btn btn-danger"><i class="oi oi-pencil"></i></button>                        
                         <script>
@@ -60,7 +76,7 @@ $consultar=mysqli_query($conectar,"SELECT *from users");
                                 });
                         </script>
                                                 </td>';
-                                                
+                                                //lo mismmo que el anterior peropara el de eliminar
                                                 echo '<td>
                                                 <button id="'.$extraido['id_user'].'" type="submit" name="no_me_quiero_ir" value="'.$extraido['id_user'].'" class="btn btn-danger"><i class="oi oi-delete"></i></button>
                                                 <script>
@@ -81,7 +97,8 @@ $consultar=mysqli_query($conectar,"SELECT *from users");
                                                 </td>';   
                                                 echo '</tr>';
                                             }
-                                    ?>    
+                                    ?>    <!--Aqui finalizo el ciclo que lee los datos de las tablas -->
+
                                         </tbody>
                                     </table>
                                 </div>
